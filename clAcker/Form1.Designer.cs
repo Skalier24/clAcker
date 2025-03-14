ebdfba2f43975b4e302a7934b3a26e32f923521a
Skalier24 <ivanrudukh@yandex.ru>
2025-03-15 03:34:16 +0800

    Первая версия

--- clAcker/Form1.Designer.cs
+++ clAcker/Form1.Designer.cs
@@ -28,34 +28,304 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Рудых Иван";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(323, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 182);
+            this.panel1.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(2, 112);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(234, 28);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Получите 100 кликов чтобы разблокировать этот скин";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(3, 84);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 20);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Бронза";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(2, 143);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Активировать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(2, 450);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(234, 28);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Найдите самый большой баг в игре";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(3, 422);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(60, 20);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Хакер";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(2, 481);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Активировать";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(2, 368);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(234, 28);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Нажмите 10000 раз чтобы получить этот скин";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label14.Location = new System.Drawing.Point(3, 340);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(104, 20);
+            this.label14.TabIndex = 11;
+            this.label14.Text = "Последняя";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(2, 399);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(104, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Активировать";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(2, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(234, 28);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Получите 1200 кликов чтобы разблокировать этот скин";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(3, 255);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Золото";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(2, 314);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Активировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(2, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(234, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Нажмите то самое кол-во раз чтобы открыть этот скин";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(3, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "WYSI";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(2, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Активировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(2, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(234, 28);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Стартовый скин";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Киви";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Активировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Segoe UI Emoji", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(386, 73);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(130, 38);
+            this.label15.TabIndex = 4;
+            this.label15.Text = "СКИНЫ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::clAcker.Properties.Resources.Kiwi_Clickjpg;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(623, 393);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label15;
+        public System.Windows.Forms.Button button1;
     }
 }
 
--- clAcker/Form1.cs
+++ clAcker/Form1.cs
@@ -1,20 +1,227 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace clAcker
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int tempClickers; //кол-во кликов в проге
+        int SavedClicks; //кол-во кликов из файла 
+        int IsBronze;
+        int IsGold;
+        int IsEG;
+        int WYSI;
+        int IsBughunter;
+
+
+        Button NowActiveBut;
+
+        Dictionary<string, int> keys = new Dictionary<string, int>(); //словарь сохраненок с файла
+
+        Dictionary<string, int> SkinOpener = new Dictionary<string, int>(); //Открывашка скинов :WW
+
+        private void Exists() //проверяет есть ли файл, если не - создает
+        {
+            FileInfo fInfo = new FileInfo("Stats.txt");
+
+            if (!fInfo.Exists)
+            {
+                using (FileStream files = new FileStream("Stats.txt", FileMode.Create))
+                {
+                    files.Close();
+                }
+            }
+            else if (fInfo.Length == 0)
+            {
+                using (StreamWriter writer = new StreamWriter("Stats.txt", true))
+                {
+                    writer.WriteLine("Clicks:0");
+                    writer.WriteLine("Kiwi:1");
+                    writer.WriteLine("Bronze:0");
+                    writer.WriteLine("Gold:0");
+                    writer.WriteLine("Endgame:0");
+                    writer.WriteLine("WYSI:0");
+                    writer.WriteLine("Hacker:0");
+                    writer.Flush();
+                }
+            }
+        }
+
+        private void DecRead()
+        {
+            string key;
+            int value;
+            using (StreamReader reader = new StreamReader("Stats.txt"))
+            {
+                string CurLine;
+                while ((CurLine = reader.ReadLine()) != null)
+                {
+                    string[] parts = CurLine.Split(':');
+                    if (parts.Length == 2)
+                    {
+                        key = parts[0].Trim();
+                        value = Convert.ToInt32(parts[1].Trim());
+
+                        if (!keys.ContainsKey(key))
+                        {
+                            keys.Add(key, value);
+                        }
+                        else
+                        {
+                            keys[key] = value;
+                        }
+                    }
+                }
+            }
+
+            keys.TryGetValue("Clicks", out SavedClicks);
+            keys.TryGetValue("Bronze", out IsBronze);
+            keys.TryGetValue("Gold", out IsGold);
+            keys.TryGetValue("Endgame", out IsEG);
+            keys.TryGetValue("WYSI", out WYSI);
+            keys.TryGetValue("Hacker", out IsBughunter);
+
+            label2.Text = SavedClicks.ToString();
+            NowActiveBut = button1;
+            NowActiveBut.Enabled = false;
+
+            if (IsBronze == 1) button4.Enabled = true; else button4.Enabled = false;
+            if (IsGold == 1) button3.Enabled = true; else button3.Enabled = false;
+            if (IsEG == 1) button6.Enabled = true; else button6.Enabled = false;    //ладно.
+            if (WYSI == 1) button2.Enabled = true; else button2.Enabled = false;
+            if (IsBughunter == 1) button5.Enabled = true; else button5.Enabled = false;
+        }
+
+
+        private void ChangeSkin(Button Changedbutton, Image image) //меняет скин 
+        {
+            if (Changedbutton.Enabled == true) //излишняя проверка пусть останется хихихаха
+            {
+                Changedbutton.Enabled = false; //нажатая кнопка отключается
+                pictureBox1.Image = image; //введеное изобр. ставится в PicBox
+                NowActiveBut.Enabled = true; //обратно включить прошлый скин
+                NowActiveBut = Changedbutton; // нажатая кнопка перетекает в "сейчас активная"
+                //MessageBox.Show(NowActiveBut.Name); //а точно работает??
+            }
+        }
+
+
+        private void DecWriter()
+        {
+            using (StreamWriter writer = new StreamWriter("Stats.txt", false))
+            {
+                writer.WriteLine($"Clicks:{tempClickers}");
+                writer.WriteLine($"Kiwi:1");
+                writer.WriteLine($"Bronze:{IsBronze}");
+                writer.WriteLine($"Gold:{IsGold}");
+                writer.WriteLine($"Endgame:{IsEG}");
+                writer.WriteLine($"WYSI:{WYSI}");
+                writer.WriteLine($"Hacker:{IsBughunter}");
+                writer.Flush();
+            }
+        }
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Size = new Size(250, 250);
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 50, pictureBox1.Location.Y + 50);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Visible = true;
+            tempClickers = Int32.Parse(label2.Text);
+
+            if (tempClickers % 10 == 0) { DecWriter(); } //при делении остаток 0, то будет сохранение в файл
+
+            tempClickers++;
+            label2.Text = tempClickers.ToString();
+            pictureBox1.Size = new Size(300, 300);
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y - 50);
+
+            if (tempClickers == SkinOpener["Bronze"] && IsBronze == 0)
+            {
+                IsBronze = 1;
+                button4.Enabled = true;
+                MessageBox.Show("Бронза разблокирована!");
+            }
+            if (tempClickers == SkinOpener["Gold"] && IsGold == 0)
+            {
+                IsGold = 1;
+                button3.Enabled = true;
+                MessageBox.Show("Золото разблокировано");
+            }
+            if (tempClickers == SkinOpener["Endgame"] && IsEG == 0)
+            {
+                IsEG = 1;
+                button6.Enabled = true;
+                MessageBox.Show("Эндгейм достигнут.");
+            }
+            if (tempClickers == SkinOpener["WYSI"] && WYSI == 0)
+            {
+                WYSI = 1;
+                button2.Enabled = true;
+                MessageBox.Show("ААА WYSI");
+            }
+            if (pictureBox1.Location.Y >= SkinOpener["Hacker"] && IsBughunter == 0)
+            {
+                IsBughunter = 1;
+                button5.Enabled = true;
+                MessageBox.Show("Поздравляю, вы Хуцкер!");
+            }
+
+            if (pictureBox1.Location.Y >= 300) pictureBox1.Location = new Point(12, 61);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Exists(); //проверяет есть ли файл, если не - создает
+            DecRead();//читает файл
+            SkinOpener.Add("Bronze", 100);
+            SkinOpener.Add("Gold", 1200);
+            SkinOpener.Add("Endgame", 10000); // для кликов
+            SkinOpener.Add("WYSI", 727);
+            SkinOpener.Add("Hacker", 300); // для Y пиктюрбокса
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ChangeSkin(button6, Properties.Resources.EG1); //МАРИЯ В ГЛАЗУРИ
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChangeSkin(button1, Properties.Resources.Kiwi_Clickjpg); //обратно киви
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ChangeSkin(button4, Properties.Resources.Bronze); //Бронза
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChangeSkin(button2, Properties.Resources.WYSI); //ААААА 727
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ChangeSkin(button5, Properties.Resources.Hacker); //че.
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChangeSkin(button3, Properties.Resources.Gold); //золото
         }
     }
 }
